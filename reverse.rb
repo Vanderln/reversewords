079b8c14a5c42eabd9ce9557fc507f11923de08b
Bill Van Der Laan <wvanderln@hotmail.com>
2013-07-24 08:07:13 -0500

    Expanded method to include 2 letter words

--- reverse.rb
+++ reverse.rb
@@ -1,5 +1,6 @@
 def rewrite_reverse(string)
-	return string
+	return string.slice!(-1) + string.slice!(0..-1)
 end
 
 puts "h".reverse == rewrite_reverse("h")
+puts "he".reverse == rewrite_reverse("he")
